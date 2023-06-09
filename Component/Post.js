140bbd7bc730da5a7f6ef97ae9d29b15a473aa41
Anamika <anamika362890@gmail.com>
2023-06-09 21:52:33 +0600

    DataFetchAndDesign

---
+++ Component/Post.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+
+const calculateDiscountPercentage = (originalPrice, discountedPrice) => {
+    const discountAmount = originalPrice - Math.abs(discountedPrice);
+    const discountPercentage = (discountAmount / originalPrice) * 100;
+    return discountPercentage.toFixed(0);
+};
+
+const Post = ({ post }) => {
+    const discountPercentage = calculateDiscountPercentage(
+        post.variations[0].price.mrp,
+        post.variations[0].price.discountedPrice
+    );
+
+    const productName = post.name.length > 55 ? post.name.slice(0, 55) + '...' : post.name;
+
+    const showDiscount = post.variations[0].price.mrp !== post.variations[0].price.discountedPrice;
+
+    const showNew = post.createdAt.slice(0, 4) === '2023';
+
+    return (
+        <div className="w-80 mx-auto">
+            <div className="bg-base-100 h-[400px] overflow-hidden">
+                <figure className="border-y border-gray-100 relative">
+
+
+                    {showNew && (
+
+                        <h1 className='absolute top-3 left-1 bg-yellow-400 text-white px-3 text-xs 
+py-0 rounded-sm font-semibold '>New</h1>
+                    )}
+
+
+
+                    {showDiscount && (
+                        <h1 className={`absolute ${showNew ? 'top-8' : 'top-3'} left-1 bg-green-600 text-white px-[13px] text-xs py-0 rounded-sm font-semibold`}>
+                            {discountPercentage}%
+                        </h1>
+                    )}
+
+                    <img src={post.productImage} alt="products" className="w-full h-64 object-cover" />
+                </figure>
+                <div className="px-6 pb-48 pt-2 h-56 flex flex-col justify-between text-center">
+                    <div>
+                        <h2 className="text-lg font-bold mb-2 h-11 overflow-hidden" style={{ letterSpacing: '-.01em', fontSize: '14px', fontWeight: 500, color: '#666', lineHeight: 1.72 }}>
+                            {productName}
+                        </h2>
+
+                        {showDiscount && (
+                            <div className="flex justify-center items-center mb-2">
+                                <h2 className="text-gray text-sm">BDT</h2>
+                                <span style={{ fontWeight: 'bold' }} className="line-through text-gray me-2 mx-1">{post.variations[0].price.mrp}</span>
+                                <h2 className="text-[#00a88a] ms-5">{discountPercentage}%</h2>
+                            </div>
+                        )}
+
+                        <h2 className="text-gray" style={{ fontWeight: 'bold', fontSize: '15.75px', color: '#666666' }}>
+                            MRP<span className="ml-1">{post.variations[0].price.discountedPrice}</span>
+                        </h2>
+
+
+
+
+                    </div>
+                    <div className="flex justify-center items-center mt-2">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Post;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import Post from '@/Component/Post';
+import React, { useState } from 'react';
 
-const index = () => {
+const index = ({ initialPosts }) => {
+  const [posts, setPosts] = useState(initialPosts);
   return (
     <div>
-      <h1>This is next js</h1>
+      <div className="container mx-auto px-24 ">
+        <h1 className="text-3xl my-4 pt-1 pb-4 text-[##444] font-bold">Products</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+          {posts.map((post) => (
+            <div key={post.id} className="w-full  flex justify-center">
+              <Post post={post} />
+            </div>
+          ))}
+        </div>
+
+      </div>
 
 
     </div>
@@ -11,3 +23,23 @@ const index = () => {
 };
 
 export default index;
+
+export const getStaticProps = async () => {
+  try {
+    const response = await fetch('https://staging-catalog-reader.qcoom.com/api/v1/product/v2?page=0&limit=10&type=Q_COMMERCE');
+    const data = await response.json();
+
+    return {
+      props: {
+        initialPosts: data.products,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching initial posts:', error);
+    return {
+      props: {
+        initialPosts: [],
+      },
+    };
+  }
+};
